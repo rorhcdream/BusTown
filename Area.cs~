0019de601076dc95a784172b15ba14f920f87e73
Jaewoong Kim <rorhcdream@gmail.com>
2018-11-17 18:36:06 +0900

    clear

--- Area.cs~
+++
@@ -1,7 +0,0 @@
-﻿using System;
-
-public enum Area
-{
-    Default
-}
-    
--- SidewalkNode/SidewalkNodeType.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-public enum SidewalkNodeType
-{
-    Default,
-    TrafficLight,
-    BusStation
-}
-    
